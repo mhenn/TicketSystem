33631193a9bc492bb6ee4662b366ec593562def9
Mike Henning <mikeianhenning93@gmail.com>
2020-09-02 21:27:53 +0200

    added css

--- services/frontend/src/components/Footer.vue
+++ services/frontend/src/components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
 	<div class="Footer">
+		<div class="flex">
+			<router-link class="ferryman" to="/" >Impressum</router-link> 
+			<router-link class="ferryman" to="/" >Datenschutz</router-link> 
+		</div>
+	
 	</div>
 </template>
 
@@ -11,4 +16,42 @@ export default {
 
 </script>
 
+<style scoped>
+	
+.Footer{
+	display:flex;
+	background-color: rgb(72,5,0);
+}
 
+a{
+	position: relative;
+	padding-bottom: 2px;
+	font-weight: 700;
+	font-family: "Roboto", sans-serif;
+	text-transform: none;
+	color: #fff;
+	text-decoration: none;
+	-webkit-transition: all 0.2s ease-in-out;
+	transition: all 0.2s ease-in-out;
+}
+
+a:after{
+	content: "";
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background-color: #fff;
+	-webkit-transform: scaleX(0);
+	transform: scaleX(0);
+	height: 2px;
+	-webkit-transition: all 0.2s ease-in-out;
+	transition: all 0.2s ease-in-out;
+}
+
+a:hover:after{
+	transform: scaleX(1);
+	background-color: red;
+}
+
+</style>
--- services/frontend/src/components/Navbar.vue
+++ services/frontend/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="nav">
-		<router-link to="/">Home</router-link> |
-		<router-link to="/about">About</router-link>
+		<router-link class="ferryman" to="/about" >About</router-link>
+		<router-link class="ferryman" to="/" >Home</router-link> 
 	</div>
 </template>
 
@@ -14,6 +14,29 @@ export default {
 </script>
 
 <style scoped>
+
+.nav{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    margin: 0;
+    height: 60px;
+    z-index: 99;
+    background-color: rgb(72, 5, 0);
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
+
+
 h3 {
   margin: 40px 0 0;
 }
@@ -26,6 +49,35 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+	position: relative;
+	padding-bottom: 2px;
+	font-weight: 700;
+	font-family: "Roboto", sans-serif;
+	text-transform: none;
+	color: #fff;
+	text-decoration: none;
+	-webkit-transition: all 0.2s ease-in-out;
+	transition: all 0.2s ease-in-out;
 }
+
+a:after{
+
+	content: "";
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background-color: #fff;
+	-webkit-transform: scaleX(0);
+	transform: scaleX(0);
+	height: 2px;
+	-webkit-transition: all 0.2s ease-in-out;
+	transition: all 0.2s ease-in-out;
+}
+
+a:hover:after{
+	transform: scaleX(1);
+	background-color: red;
+}
+
 </style>
